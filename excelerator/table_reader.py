34bf4015558d42c42e7e7c9f2b179303fd1dd602
Jonathan Chukinas (work computer) <jchukina@its.jnj.com>
2019-10-30 14:29:59 -0400

    add fields param to TableReader
    uprev

--- excelerator/table_reader.py
+++ excelerator/table_reader.py
@@ -1,30 +1,58 @@
+"""table_reader docstring"""
+
+# --- Standard Library Imports ------------------------------------------------
+from contextlib import contextmanager
+
+# --- Third Party Imports -----------------------------------------------------
+# None
+
+# --- Intra-Package Imports ---------------------------------------------------
 from excelerator import utils
-from excelerator.worksheet_rows import get_worksheet_row
+from excelerator import headers
 
 
 class TableReader:
 
-    def __init__(self, header_row_num=None):
+    def __init__(
+            self,
+            fields=None,
+            header_row_num=None,
+    ):
+        self._ws = None
+        self.fields = fields
         self.header_row_num = header_row_num
 
     def read_from(self, path, sheetname):
         ws = utils.get_worksheet_from_path(path, sheetname)
-        result = dict()
-        headers = get_worksheet_row(
-            worksheet=ws,
-            row_int=self.header_row_num,
-        )
-        for col_num, header in enumerate(headers, 1):
-            if header is not None:
-                result[header] = utils.get_column(worksheet=ws,
-                                                  col_num=col_num,
-                                                  row_start=self.records_row_start,
-                                                  row_end=ws.max_row)
-        return result
+        with self._set_ws(ws):
+            result = dict()
+            headers = utils.get_worksheet_row(
+                worksheet=ws,
+                row_int=self.header_row_num,
+            )
+            for col_num, header in enumerate(headers, 1):
+                if self._valid_header(header):
+                    result[header] = utils.get_column(worksheet=ws,
+                                                      col_num=col_num,
+                                                      row_start=self.records_row_start,
+                                                      row_end=ws.max_row)
+            return result
+
+    def _valid_header(self, value):
+        if self.fields:
+            return value in self.fields
+        else:
+            return value is not None
 
     @property
     def header_row_num(self):
-        return self._header_row_num
+        if self._ws is not None and self.fields is not None:
+            return headers.get_best_match_row_number(
+                worksheet=self._ws,
+                values=self.fields,
+            )
+        else:
+            return self._header_row_num
 
     @header_row_num.setter
     def header_row_num(self, value):
@@ -36,3 +64,9 @@ class TableReader:
     @property
     def records_row_start(self):
         return self.header_row_num + 1
+
+    @contextmanager
+    def _set_ws(self, value):
+        self._ws = value
+        yield
+        self._ws = None
--- tests/test_table_reader.py
+++ tests/test_table_reader.py
@@ -1,30 +1,10 @@
 import pytest
 from excelerator import TableReader
 from excelerator import exceptions
+from  collections import namedtuple
 
 
-simple_table_expected_output = {
-    'first_name': 'Rose Amy River'.split(),
-    'last_name': 'Tyler Pond Song'.split(),
-    'last_appearance': [2013, 2013, 2015],
-}
-
-
-@pytest.mark.parametrize('sheetname', 'table_top_left table_top_right'.split())
-def test_table_simple_top(sheetname, fixture_path):
-    """Test the most simple use case - default table interpretation"""
-    tr = TableReader()
-    actual_output = tr.read_from(path=fixture_path, sheetname=sheetname)
-    assert actual_output == simple_table_expected_output
-
-
-@pytest.mark.parametrize('sheetname', 'table_bottom_left table_bottom_right'.split())
-def test_table_simple_top(sheetname, fixture_path):
-    """Test another simple use case - default table interpretation + header_row_num"""
-    tr = TableReader(header_row_num=4)
-    actual_output = tr.read_from(path=fixture_path, sheetname=sheetname)
-    assert actual_output == simple_table_expected_output
-
+# --- path and sheetname errors -----------------------------------------------
 
 def test_table_reader_invalid_extension(fixture_path):
     path_with_incorrect_extension = fixture_path.parent / 'not_excel.docx'
@@ -45,3 +25,76 @@ def test_table_reader_missing_worksheet(fixture_path):
         return
     assert False
 
+
+# --- read simple table -------------------------------------------------------
+
+simple_table_expected_output = {
+    'first_name': 'Rose Amy River'.split(),
+    'last_name': 'Tyler Pond Song'.split(),
+    'last_appearance': [2013, 2013, 2015],
+}
+
+
+WorksheetGiven = namedtuple('WorksheetGiven', 'sheetname header_row_num')
+worksheetgivens = [
+    WorksheetGiven('table_top_left', 1),
+    WorksheetGiven('table_top_right', 1),
+    WorksheetGiven('table_bottom_left', 4),
+    WorksheetGiven('table_bottom_right', 4),
+]
+
+
+sheetnames_with_headers_in_first_row = [
+    wsgiven.sheetname
+    for wsgiven in worksheetgivens
+    if wsgiven.header_row_num == 1
+]
+@pytest.mark.parametrize('sheetname', sheetnames_with_headers_in_first_row)
+def test_table_simple_top(sheetname, fixture_path):
+    """Test the most simple use case - default table interpretation"""
+    tr = TableReader()
+    actual_output = tr.read_from(path=fixture_path, sheetname=sheetname)
+    assert actual_output == simple_table_expected_output
+
+
+@pytest.mark.parametrize('worksheet_given', worksheetgivens)
+def test_table_simple_top(worksheet_given: WorksheetGiven, fixture_path):
+    """Test another simple use case - default table interpretation + header_row_num"""
+    sheetname, header_row_num = worksheet_given
+    tr = TableReader(header_row_num=header_row_num)
+    actual_output = tr.read_from(path=fixture_path, sheetname=sheetname)
+    assert actual_output == simple_table_expected_output
+
+
+def without(d, key):
+    new_d = d.copy()
+    new_d.pop(key)
+    return new_d
+
+
+FieldsAndResult = namedtuple('FieldsAndResult', 'fields expected_result')
+fields_and_results = [
+    FieldsAndResult('first_name last_name last_appearance'.split(), simple_table_expected_output),
+    FieldsAndResult('first_name last_name last_aperants'.split(),
+                    without(simple_table_expected_output, 'last_appearance')),
+    FieldsAndResult('first_name last_name'.split(),
+                    without(simple_table_expected_output, 'last_appearance')),
+]
+
+
+@pytest.mark.parametrize('worksheet_given', worksheetgivens)
+@pytest.mark.parametrize('fields_results', fields_and_results)
+def test_table_simple_seek_header(worksheet_given: WorksheetGiven, fields_results: FieldsAndResult, fixture_path):
+
+    # GIVEN a spreadsheet whose header row may/not start in cell 'A1'...
+    sheetname, header_row_num = worksheet_given
+
+    # WHEN a TableReader is instantiated with a sequence of fields
+    fields = fields_results.fields
+    tr = TableReader(fields = fields)
+
+    # THEN the header row is found automatically and only those fields passed
+    #   as arguments are returned.
+    actual_output = tr.read_from(path=fixture_path, sheetname=sheetname)
+    expected_output = fields_results.expected_result
+    assert actual_output == expected_output
